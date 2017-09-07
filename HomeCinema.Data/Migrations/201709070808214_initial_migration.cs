05c139bffcf31c33d96506b8880c6674841c1c6a
evgeniy.skiba <skiba.evgeniy.test@gmail.com>
2017-09-07 20:09:30 +0300

    changed migrations script

--- HomeCinema.Data/Migrations/201709070808214_initial_migration.cs
+++ HomeCinema.Data/Migrations/201709070808214_initial_migration.cs
@@ -10,126 +10,126 @@ namespace HomeCinema.Data.Migrations
             CreateTable(
                 "dbo.Customer",
                 c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        FirstName = c.String(nullable: false, maxLength: 100),
-                        LastName = c.String(nullable: false, maxLength: 100),
-                        Email = c.String(nullable: false, maxLength: 200),
-                        IdentityCard = c.String(nullable: false, maxLength: 50),
-                        UniqueKey = c.Guid(nullable: false),
-                        DateOfBirth = c.DateTime(nullable: false),
-                        Mobile = c.String(maxLength: 10),
-                        RegistrationDate = c.DateTime(nullable: false),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    FirstName = c.String(nullable: false, maxLength: 100),
+                    LastName = c.String(nullable: false, maxLength: 100),
+                    Email = c.String(nullable: false, maxLength: 200),
+                    IdentityCard = c.String(nullable: false, maxLength: 50),
+                    UniqueKey = c.Guid(nullable: false),
+                    DateOfBirth = c.DateTime(nullable: false),
+                    Mobile = c.String(maxLength: 10),
+                    RegistrationDate = c.DateTime(nullable: false),
+                })
                 .PrimaryKey(t => t.ID);
-            
+
             CreateTable(
                 "dbo.Error",
                 c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        Message = c.String(),
-                        StackTrace = c.String(),
-                        DateCreated = c.DateTime(nullable: false),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    Message = c.String(),
+                    StackTrace = c.String(),
+                    DateCreated = c.DateTime(nullable: false),
+                })
                 .PrimaryKey(t => t.ID);
-            
+
             CreateTable(
                 "dbo.Genre",
                 c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        Name = c.String(nullable: false, maxLength: 50),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false, maxLength: 50),
+                })
                 .PrimaryKey(t => t.ID);
-            
+
             CreateTable(
                 "dbo.Movie",
                 c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        Title = c.String(nullable: false, maxLength: 100),
-                        Description = c.String(nullable: false, maxLength: 2000),
-                        Image = c.String(),
-                        GenreId = c.Int(nullable: false),
-                        Director = c.String(nullable: false, maxLength: 100),
-                        Writer = c.String(nullable: false, maxLength: 50),
-                        Producer = c.String(nullable: false, maxLength: 50),
-                        ReleaseDate = c.DateTime(nullable: false),
-                        Rating = c.Byte(nullable: false),
-                        TrailerURI = c.String(maxLength: 200),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    Title = c.String(nullable: false, maxLength: 100),
+                    Description = c.String(nullable: false, maxLength: 2000),
+                    Image = c.String(),
+                    GenreId = c.Int(nullable: false),
+                    Director = c.String(nullable: false, maxLength: 100),
+                    Writer = c.String(nullable: false, maxLength: 50),
+                    Producer = c.String(nullable: false, maxLength: 50),
+                    ReleaseDate = c.DateTime(nullable: false),
+                    Rating = c.Byte(nullable: false),
+                    TrailerURI = c.String(maxLength: 200),
+                })
                 .PrimaryKey(t => t.ID)
                 .ForeignKey("dbo.Genre", t => t.GenreId, cascadeDelete: true)
                 .Index(t => t.GenreId);
-            
+
             CreateTable(
                 "dbo.Stock",
                 c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        MovieId = c.Int(nullable: false),
-                        UniqueKey = c.Guid(nullable: false),
-                        IsAvailable = c.Boolean(nullable: false),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    MovieId = c.Int(nullable: false),
+                    UniqueKey = c.Guid(nullable: false),
+                    IsAvailable = c.Boolean(nullable: false),
+                })
                 .PrimaryKey(t => t.ID)
                 .ForeignKey("dbo.Movie", t => t.MovieId, cascadeDelete: true)
                 .Index(t => t.MovieId);
-            
+
             CreateTable(
                 "dbo.Rental",
                 c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        CustomerId = c.Int(nullable: false),
-                        StockId = c.Int(nullable: false),
-                        RentalDate = c.DateTime(nullable: false),
-                        ReturnedDate = c.DateTime(),
-                        Status = c.String(nullable: false, maxLength: 10),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    CustomerId = c.Int(nullable: false),
+                    StockId = c.Int(nullable: false),
+                    RentalDate = c.DateTime(nullable: false),
+                    ReturnedDate = c.DateTime(),
+                    Status = c.String(nullable: false, maxLength: 10),
+                })
                 .PrimaryKey(t => t.ID)
                 .ForeignKey("dbo.Stock", t => t.StockId, cascadeDelete: true)
                 .Index(t => t.StockId);
-            
+
             CreateTable(
                 "dbo.Role",
                 c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        Name = c.String(nullable: false, maxLength: 50),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false, maxLength: 50),
+                })
                 .PrimaryKey(t => t.ID);
-            
+
             CreateTable(
                 "dbo.UserRole",
                 c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        UserId = c.Int(nullable: false),
-                        RoleId = c.Int(nullable: false),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    UserId = c.Int(nullable: false),
+                    RoleId = c.Int(nullable: false),
+                })
                 .PrimaryKey(t => t.ID)
                 .ForeignKey("dbo.Role", t => t.RoleId, cascadeDelete: true)
                 .ForeignKey("dbo.User", t => t.UserId, cascadeDelete: true)
                 .Index(t => t.UserId)
                 .Index(t => t.RoleId);
-            
+
             CreateTable(
                 "dbo.User",
                 c => new
-                    {
-                        ID = c.Int(nullable: false, identity: true),
-                        Username = c.String(nullable: false, maxLength: 100),
-                        Email = c.String(nullable: false, maxLength: 200),
-                        HashedPassword = c.String(nullable: false, maxLength: 200),
-                        Salt = c.String(nullable: false, maxLength: 200),
-                        IsLocked = c.Boolean(nullable: false),
-                        DateCreated = c.DateTime(nullable: false),
-                    })
+                {
+                    ID = c.Int(nullable: false, identity: true),
+                    Username = c.String(nullable: false, maxLength: 100),
+                    Email = c.String(nullable: false, maxLength: 200),
+                    HashedPassword = c.String(nullable: false, maxLength: 200),
+                    Salt = c.String(nullable: false, maxLength: 200),
+                    IsLocked = c.Boolean(nullable: false),
+                    DateCreated = c.DateTime(nullable: false),
+                })
                 .PrimaryKey(t => t.ID);
-            
+
         }
-        
+
         public override void Down()
         {
             DropForeignKey("dbo.UserRole", "UserId", "dbo.User");
@@ -152,7 +152,5 @@ namespace HomeCinema.Data.Migrations
             DropTable("dbo.Error");
             DropTable("dbo.Customer");
         }
-
-        
     }
 }
---
+++ HomeCinema.Data/MigrationsReadme.txt
@@ -0,0 +1,8 @@
+﻿
+1 enable-migrations
+
+
+2 add-migration "initial_migration"
+
+3  update-database -verbose
+
